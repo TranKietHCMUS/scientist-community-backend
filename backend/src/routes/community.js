670be1629473d7c1222f5af002cc5ddcd12b114d
Kiệt Trần <anhkiet10h1nd@gmail.com>
2024-08-22 09:49:43 +0700

    update route

--- backend/src/routes/community.js
+++ backend/src/routes/community.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {getCommunityList, createCommunity, getCommunityDetail} = require("../controllers/community");
-const {verifyToken} = require("../middlewares/verify-token")
+const {verifyToken} = require("../middlewares/verify-token");
 
-router.get("/", verifyToken, getCommunityList);
-router.post("/", verifyToken, createCommunity);
+router.use(verifyToken);
+
+router.route("/")
+    .get(getCommunityList)
+    .post(createCommunity);
+
 router.get("/detail/:id", verifyToken, getCommunityDetail);
 
 module.exports = router;
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const {updateProfileUser, getProfileUser, updateAvatarUser, getAvatarUser} = require("../controllers/user");
+const {updateProfileUser, 
+    getProfileUser, 
+    updateAvatarUser, 
+    getAvatarUser} = require("../controllers/user");
 const {verifyToken} = require("../middlewares/verify-token");
 const storage = require("../configs/multer");
   
 const upload = multer({ storage });
 
-router.patch("/", verifyToken, updateProfileUser);
-router.get("/", verifyToken, getProfileUser);
-router.post("/avatar", verifyToken, upload.single('file'), updateAvatarUser);
-router.get("/avatar", verifyToken, getAvatarUser);
+router.use(verifyToken);
+
+router.route("/")
+    .get(getProfileUser)
+    .patch(updateProfileUser);
+
+router.route("/avatar")
+    .get(getAvatarUser)
+    .post(upload.single('file'), updateAvatarUser);
 
 module.exports = router;
